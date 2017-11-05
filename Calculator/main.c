af07f604c5d3e88d0ab6d1a9d22e7698e1138c07
Walim <machine-hb@hotmail.fr>
2017-11-05 16:26:43 +0100

    Soustraction

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition(int a, int b)
+int soustraction(int a, int b)
 {
-    return a+b;
+    return a-b;
 }
 
 int main()
 {
-<<<<<<< HEAD
-    printf("a+b=%d\n", addition(1,4));
-=======
-    printf("L’addition de a et de b vaut %d\n", addition(1,4)); 
-
->>>>>>> b5179fa713ad80b5342414f9a3e2a6638e9b93fa
+    printf("La soustraction de a et de b vaut %d\n", soustraction(1,4));
     return 0;
 }
